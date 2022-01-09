181428fc17d4f41aea384b977e662b1c80c378b7
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-01-09 01:00:37 -0800

    add function to alert user

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
-$(document).ready(function () {
-    $("#design-cont").click(function () {
-        $("#design-img").toggle();
-        $(".design").toggle();
-    });
-    $("#develop-cont").click(function () {
-        $("#develop-img").toggle();
-        $(".develop").toggle();
-    });
-    $("#product-cont").click(function () {
-        $("#product-img").toggle();
-        $(".product").toggle();
-    });
-});
+ //jQuery functions
+ $(document).ready(function () {
+     $("#design-cont").click(function () {
+         $("#design-img").toggle();
+         $(".design").toggle();
+     });
+     $("#develop-cont").click(function () {
+         $("#develop-img").toggle();
+         $(".develop").toggle();
+     });
+     $("#product-cont").click(function () {
+         $("#product-img").toggle();
+         $(".product").toggle();
+     });
+
+     $("#project-a, #project-b, #project-c, #project-d, #project-e, #project-f, #project-g, #project-h").hover(function () {
+         $(this).find(".project").fadeToggle("10");
+     });
+
+ });
+
+ //JS functions
+ function alertUser() {
+     let name = document.getElementById("name").value.toUpperCase();
+     let message = `, we have received your message. Thank You for contacting us.`;
+     alert(`Hello ${name} ${message}`);
+ }
